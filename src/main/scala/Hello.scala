522572f572cf6aba0f809310476a3f4f0a50d6bd
Ben Leib <bleib@twitter.com>
2015-12-21 14:13:18 -0600

    Stuff

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,5 +1,10 @@
-object Hello {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
-  }
+import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.http.HttpServer
+
+object HomeServerMain extends HomeServer
+
+class HomeServer extends HttpServer {
+  override val modules = Nil
+
+  override def configureHttp(router: HttpRouter) = { }
 }
