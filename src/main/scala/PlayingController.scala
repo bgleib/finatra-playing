c30e61451d16c123a6f405bcfe2ceecd7f78e8a2
Ben Leib <bleib@twitter.com>
2015-12-21 15:23:34 -0600

    Say hi

--- src/main/scala/PlayingController.scala
+++ src/main/scala/PlayingController.scala
@@ -2,7 +2,7 @@ import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
 
 class PlayingController extends Controller {
-  get("/ping") { request: Request =>
-    "pong"
+  get("/hello") { request: Request =>
+    "Hello Monica!"
   }
 }
