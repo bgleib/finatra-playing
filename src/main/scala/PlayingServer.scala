d25811ab2f8a53c4f989cabd6f8f4a411cbd2fa3
Ben Leib <bleib@twitter.com>
2015-12-21 15:23:11 -0600

    More server setup

--- src/main/scala/PlayingServer.scala
+++ src/main/scala/PlayingServer.scala
@@ -1,13 +1,22 @@
-import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finagle.http.{Response, Request}
 import com.twitter.finatra.http.HttpServer
+import com.twitter.finatra.http.filters.CommonFilters
+import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.logging.filter.{LoggingMDCFilter, TraceIdMDCFilter}
+import com.twitter.finatra.logging.modules.Slf4jBridgeModule
 
 object PlayingServerMain extends PlayingServer
 
 class PlayingServer extends HttpServer {
+
   override val disableAdminHttpServer = true
-  override val modules = Nil
+  override val modules = Seq(Slf4jBridgeModule)
 
   override def configureHttp(router: HttpRouter) = {
-    router.add[PlayingController]
+    router
+      .filter[LoggingMDCFilter[Request, Response]]
+      .filter[TraceIdMDCFilter[Request, Response]]
+      .filter[CommonFilters]
+      .add[PlayingController]
   }
 }
