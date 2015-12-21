597f5a9704fe09d422fb96e5de65426195802403
Ben Leib <bleib@twitter.com>
2015-12-21 16:43:45 -0600

    Templating works

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: target/universal/stage/bin/finatra-playing -- -admin.port=:$PORT -http.port=:$PORT -local.doc.root="src/main/resources"
+web: target/universal/stage/bin/finatra-playing -- -admin.port=:$PORT -http.port=:$PORT -local.doc.root="src/main/resources" -mustache.templates.dir="templates"
--- src/main/scala/PlayingController.scala
+++ src/main/scala/PlayingController.scala
@@ -1,10 +1,18 @@
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.finatra.response.Mustache
 
 class PlayingController extends Controller {
   get("/hello") { request: Request =>
     info("Hi was called")
     "Hello Monica!"
+  }
+
+  @Mustache("home")
+  case class HomeView(message: String)
+
+  get("/home") { request: Request =>
+    HomeView("Blah blah")
   }
 
   get("/pic") { request: Request =>
